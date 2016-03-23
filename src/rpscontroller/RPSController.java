60a9f856ff810a51d6b4329c8f9a7a276a652c41
josiahelmer <jelm9149@740s-pl-112.local>
2016-03-23 12:14:58 -0600

    Start up

---
+++ src/rpscontroller/RPSController.java
@@ -0,0 +1,21 @@
+package rpscontroller;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class RPSController
+{
+	private String Rock;
+	private String Paper;
+	private String Scissors;
+	
+	public RPSController()
+	{
+		
+	}
+
+	public void start()
+	{
+		
+	}
+}
---
+++ src/rpscontroller/RPSRunner.java
@@ -0,0 +1,10 @@
+package rpscontroller;
+
+public class RPSRunner
+{
+	public static void main(String args)
+	{
+		RPSController myRPSController = new RPSController();
+		myRPSController.start();
+	}
+}
